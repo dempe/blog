5497c78992b03d5213a52bc46ce23d81fb3f65b5
Christopher Dempewolf <chris@chrisdempewolf.com>
2023-05-17 20:35:20 -0500

    Make PostTag model and migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,5 +10,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PostSeeder::class);
         $this->call(TagSeeder::class);
+        $this->call(PostTagSeeder::class);
     }
 }
