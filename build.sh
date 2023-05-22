daeffea1c43949c1e5e91a02894b356c88b49423
Christopher Dempewolf <chris@chrisdempewolf.com>
2023-05-22 09:54:17 -0500

    Build: 8971699 Build: 62d67f1 Build: f35e46e Add build script

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ wget --directory-prefix=output/ --html-extension -k -r -l 10 -p -N -F -nH http:/
 rm -rf ./output/cdn-cgi/
 
 # Remove ".html" extension from files
-for f in $(find ./output -type f ! -name '.git'); do LC_CTYPE=C && LANG=C && mv "$f" $(echo $f | sed 's/\.html//'); done
+# for f in $(find ./output -type f ! -name '.git'); do LC_CTYPE=C && LANG=C && mv "$f" $(echo $f | sed 's/\.html//'); done
 
 # Rename links within files to match extensionless format
 # TODO: convert this whole script to Python and go through each file above and rename each link for each file.  This will break any appearance of ".html" that is not an internal link.
 #
-for f in $(find ./output -type f ! -name '.git'); do LC_CTYPE=C && LANG=C && sed -i '' 's/\.html//g' "$f"; done
+# for f in $(find ./output -type f ! -name '.git'); do LC_CTYPE=C && LANG=C && sed -i '' 's/\.html//g' "$f"; done
 
 git add output/* 
 git commit -am "Build: $(git log --oneline | head -n 1)"
