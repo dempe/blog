b7aa1f05fcaf86c67c9e8a80d1ed2efdd78e1b6b
Christopher Dempewolf <chris@chrisdempewolf.com>
2023-07-10 22:58:53 -0600

    Build: 5ca22e8 Rearrange post index tags

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ echo "Logging to $log_file"
 
 rm -rf ./output
 mkdir ./output
-wget --directory-prefix=output/ --html-extension --convert-links --recursive --level=10 --page-requisites --timestamping --adjust-extension --no-host-directories http://localhost:8000 | tee -a "$log_file"
+wget --directory-prefix=output/ --html-extension --convert-links --recursive --level=10 --page-requisites --timestamping --adjust-extension --no-host-directories http://localhost:8000 |& tee -a "$log_file"
 
 # Grab the 404 page
-wget --directory-prefix=output/ --html-extension --convert-links --timestamping --adjust-extension --no-host-directories http://localhost:8000/404 | tee -a "$log_file"
+wget --directory-prefix=output/ --html-extension --convert-links --timestamping --adjust-extension --no-host-directories http://localhost:8000/404 |& tee -a "$log_file"
 
 # Grab the general error page
-wget --directory-prefix=output/ --html-extension --convert-links --timestamping --adjust-extension --no-host-directories http://localhost:8000/error | tee -a "$log_file"
+wget --directory-prefix=output/ --html-extension --convert-links --timestamping --adjust-extension --no-host-directories http://localhost:8000/error |& tee -a "$log_file"
 
 # For some reason Laravel adds this.  It is for Cloudflare, but things get wonky if I upload this.
 rm -rf ./output/cdn-cgi/
