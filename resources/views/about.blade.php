9424258f440a4917452f7cb3445e0fcb323b31f7
Christopher Dempewolf <chris@chrisdempewolf.com>
2024-03-03 11:51:21 -0600

    Update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,19 +8,24 @@
         Hello! I'm Chris Dempewolf (dim-pee-wolf), software engineer and former ESL teacher.
     </p>
 
-    {{--    <figure>--}}
-    {{--        <img src="/assets/img/me-selfie-cancun.jpg"--}}
-    {{--             alt="A selfie I took in Cancún.  I squint as the sun shines brightly overhead.  There is a palm tree behind me."--}}
-    {{--             width="200"--}}
-    {{--             title="The author in Cancún"/>--}}
-    {{--        <figcaption>The author in Cancún</figcaption>--}}
-    {{--    </figure>--}}
-
     <p>
         I was born in the Fort Smith, Arkansas, USA. I lived in Seattle for 2 years and
         abroad in Mexico, Japan, and Germany. I taught English for a couple of years and have worked as a software
         engineer for 7 years.
     </p>
+
+    <p>
+        This is what I look like:
+    </p>
+
+    <figure>
+            <img src="/assets/img/me-selfie-cancun.jpg"
+                 alt="A selfie I took in Cancún.  I squint as the sun shines brightly overhead.  There is a palm tree behind me."
+                 width="200"
+                 title="The author in Cancún"/>
+            <figcaption>At the beach</figcaption>
+        </figure>
+
 
 
     {{--    <p>Some of my hobbies are ... </p>--}}
@@ -35,30 +40,19 @@
     {{--        <li><strong>Video games</strong> 🎮</li>--}}
     {{--    </ul>--}}
 
-    {{--    <figure>--}}
-    {{--        <img src="/assets/img/me-pacman.jpg"--}}
-    {{--             alt="Here, I'm playing Ms. PacMan on an arcade table in a dark Seattle bar. Turns out, they make arcade game tables so you can sit and do whatever.  In this case, drink a beer with a friend."--}}
-    {{--             width="200"--}}
-    {{--             title="The author playing Ms. PacMan"/>--}}
-    {{--        <figcaption>The author playing Ms. PacMan</figcaption>--}}
-    {{--    </figure>--}}
+        <figure>
+            <img src="/assets/img/me-pacman.jpg"
+                 alt="Here, I'm playing Ms. PacMan on an arcade table in a dark Seattle bar. Turns out, they make arcade game tables so you can sit and do whatever.  In this case, drink a beer with a friend."
+                 width="200"
+                 title="The author playing Ms. PacMan"/>
+            <figcaption>Playing Ms. PacMan</figcaption>
+        </figure>
 
     <p>
-        I created this blog for two reasons:
+        Thanks for stopping by!
     </p>
 
-    <ol>
-        <li>To share what I learn</li>
-        <li>To help me learn</li>
-    </ol>
 
-    <p>
-        I don't really know what I know until I write about it, so this blog is meant to serve as my light in the dark.
-        I write about computation, biology, cooking, and everything else that piques my interest. View an archive of articles I've written <a
-            href="/">here</a>.
-    </p>
-
-    <p>Thanks for stopping by!</p>
 @endsection
 @section('footer-content')
     <div class="footer-content">
