23d5b3ddd59808f1a7e181e7ae38fedb77c67414
Christopher Dempewolf <chris@chrisdempewolf.com>
2024-03-05 20:52:26 -0600

    Update log file name in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
 
+prev_commit=$(git log --oneline | head -n 1)
+prev_commit_hash="$(echo $prev_commit | awk '{print $1}')"
+log_file="./storage/logs/build_$prev_commit_hash.log"
+
 aws s3 sync ./output s3://chrisdempewolf.com --delete | tee -a "$log_file"
