2e0a955a9f72dde7da779e4423ff941ec8835b9e
Christopher Dempewolf <chris@chrisdempewolf.com>
2024-03-12 03:39:22 -0600

    Add 404 to main wget command

--- build.sh
+++ build.sh
@@ -12,11 +12,7 @@ php artisan db:seed |& tee -a "$log_file"
 
 # rm -rf ./output
 # mkdir ./output
-wget --directory-prefix=output/ --html-extension --convert-links --recursive --level=10 --page-requisites --timestamping --adjust-extension --no-host-directories http://localhost:8000 |& tee -a "$log_file"
-
-# # Grab the 404 and error page
-wget --directory-prefix=output/ --html-extension --timestamping --adjust-extension --no-host-directories http://localhost:8000/404 |& tee -a "$log_file"
-# wget --directory-prefix=output/ --html-extension --convert-links --timestamping --adjust-extension --no-host-directories http://localhost:8000/error |& tee -a "$log_file"
+wget --directory-prefix=output/ --html-extension --convert-links --recursive --level=10 --page-requisites --timestamping --adjust-extension --no-host-directories http://localhost:8000 http://localhost:8000/404 |& tee -a "$log_file"
 
 # For some reason Laravel adds this.  It is for Cloudflare, but things get wonky if I upload this.
 rm -rf ./output/cdn-cgi/
