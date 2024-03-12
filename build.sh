6359ed950573b599c0ade567ae997a055d3bbfbf
Christopher Dempewolf <chris@chrisdempewolf.com>
2024-03-12 02:21:50 -0600

    Remove --convert-links from 404 DL command

--- build.sh
+++ build.sh
@@ -14,17 +14,18 @@ php artisan db:seed |& tee -a "$log_file"
 # mkdir ./output
 wget --directory-prefix=output/ --html-extension --convert-links --recursive --level=10 --page-requisites --timestamping --adjust-extension --no-host-directories http://localhost:8000 |& tee -a "$log_file"
 
+# # Grab the 404 and error page
+wget --directory-prefix=output/ --html-extension --timestamping --adjust-extension --no-host-directories http://localhost:8000/404 |& tee -a "$log_file"
+# wget --directory-prefix=output/ --html-extension --convert-links --timestamping --adjust-extension --no-host-directories http://localhost:8000/error |& tee -a "$log_file"
+
+# For some reason Laravel adds this.  It is for Cloudflare, but things get wonky if I upload this.
+rm -rf ./output/cdn-cgi/
+
 # Build stats page
 aws s3 sync s3://logs-cloudfront-chrisdempewolf.com cloudfront-logs/ |& tee -a "$log_file"
 gunzip -c cloudfront-logs/* | goaccess -o ./output/stats.html --log-format CLOUDFRONT --time-format CLOUDFRONT --date-format CLOUDFRONT |& tee -a "$log_file"
 # rg --no-filename --no-line-number 'WEBSITE.GET.OBJECT' | goaccess -o ./output/stats.html --date-format=%d/%b/%Y --time-format=%T --log-format='%^ %^ [%d:%t %^] %h %^ %^ %^ %^ "%m %U %H" %s %^ %b %^ %T %^ %R %u %^ %^ %^ %^ %^ %v %K %^ %^' |& tee -a "$log_file"
 
-# # Grab the 404 and error page
-wget --directory-prefix=output/ --html-extension --convert-links --timestamping --adjust-extension --no-host-directories http://localhost:8000/404 |& tee -a "$log_file"
-# wget --directory-prefix=output/ --html-extension --convert-links --timestamping --adjust-extension --no-host-directories http://localhost:8000/error |& tee -a "$log_file"
-
-# For some reason Laravel adds this.  It is for Cloudflare, but things get wonky if I upload this.
-rm -rf ./output/cdn-cgi/
 
 git add output | tee -a "$log_file"
 git commit -am "Build: $prev_commit" | tee -a "$log_file"
