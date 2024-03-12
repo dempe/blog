ee424f8af247fd8c7a207df90ee3c4a7d6ba3581
Christopher Dempewolf <chris@chrisdempewolf.com>
2024-03-12 12:49:35 -0600

    Make 404 dynamic

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -4,7 +4,14 @@
     404
 @endsection
 @section('content')
-    <p>
-        This page doesn't exist!
-    </p>
+    <p id="404-message"></p>
+    <script>
+        const p = document.getElementById('404-message');
+        const codeNode = document.createElement('code');
+
+        codeNode.textContent = window.location.href;
+        p.appendChild(document.createTextNode('The URL you entered ('));
+        p.appendChild(codeNode);
+        p.appendChild(document.createTextNode(') does not exist on this site.'));
+    </script>
 @endsection
