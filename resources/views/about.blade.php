b7551d64eff5e4ea8b27c78fcb7dd84e1793569b
Christopher Dempewolf <chris@chrisdempewolf.com>
2024-07-05 07:28:27 -0600

    Update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,30 +4,19 @@
     About
 @endsection
 @section('content')
+    <h2 id="me"><a class="no-underline text-stone-200 hover:text-chartreuse" href="#me">Me</a></h2>
     <p>
-        Hello! I'm Chris Dempewolf (dim-pee-wolf), software engineer and former ESL teacher.
+        Hello! I'm Chris Dempewolf (dim-pee-wolf), software engineer, former ESL teacher, and, most recently, father.
     </p>
     <p>
-        I'm from <a href="https://en.wikipedia.org/wiki/Fort_Smith,_Arkansas">Fort Smith</a>, Arkansas. I lived in Seattle for 2 years and abroad in Mexico, Japan, and Germany. I taught English for a couple of years and have worked as a software engineer for 7 years.
+        I'm from <a href="https://en.wikipedia.org/wiki/Fort_Smith,_Arkansas">Fort Smith</a>, Arkansas. I lived in Seattle for 2 years and abroad in Mexico 🇲🇽, Japan 🇯🇵, a. I taught English for a couple of years and have worked as a software engineer for 7 years.
     </p>
-    <p>
-        This is what I look like:
-    </p>
-    <figure>
-        <img src="/assets/img/me-selfie-cancun.jpg"
-             alt="A selfie I took at a beach.  I squint as the sun shines brightly overhead.  I'm flanked by a palm tree."
-             width="200"
-             title="The author at a tropical Mexican beach"/>
-        <figcaption>At the beach</figcaption>
-    </figure>
-    <figure>
-        <img src="/assets/img/me-pacman.jpg"
-             alt="Here, I'm playing Ms. PacMan on an arcade table in a dark Seattle bar. Turns out, they make arcade game tables so you can sit and do whatever while you play - in this case, drink a beer with a friend."
-             width="200"
-             title="The author playing Ms. PacMan"/>
-        <figcaption>Playing Ms. PacMan</figcaption>
-    </figure>
-    <p>
-        Thanks for stopping by!
-    </p>
+    <h2 id="the-title"><a class="no-underline text-stone-200 hover:text-chartreuse" href="#the-title">The Title</a></h2>
+		<figure class="blockquote">
+				<blockquote>
+						<p>In the beginner's mind, there are many possibilities; in the expert's mind there are few.</p>
+				</blockquote>
+				<figcaption>Shunryu Suzuki, <em>Zen Mind, Beginner's Mind</em></figcaption>
+		</figure>
+    <p>This blog's title, <em>Shosin</em>, is a Zen concept roughly translated as "beginner's mind", a concept first popularized in the West by Zen teacher <a href="https://en.wikipedia.org/wiki/Shunry%C5%AB_Suzuki">Shunryu Suzuki</a> in his book, <em><a href="">Zen Mind, Beginner's Mind</a></em>. It implies curiosity, openness, and approaching things without preconceptions.  It is the goal of this blog to explore many new and interesting topics—always through the eyes of a beginner.</p>
 @endsection
