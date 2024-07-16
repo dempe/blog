e21e66dbcedff22301e6a98cc4b2703b467d4759
Christopher Dempewolf <chris@chrisdempewolf.com>
2024-07-15 21:44:28 -0600

    Desaturate blue; underline h2 on hover; intervariable font

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,6 +11,11 @@
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
     <link rel="icon" href="/assets/img/favicon.png" type="image/png">
     <title>Chris Dempewolf's Blog</title>
+    <style>
+        body {
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
 </head>
 <body class="antialiased box-border text-lg md:text-base bg-[#000000] text-[#cccccc] font-sans mx-auto max-w-[95%] xl:w-[1024px] mt-4 mb-8">
 <header class="flex flex-col mb-6">
